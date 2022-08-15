d0be897056d5539f9be815f024b53c6984022039
kantoash <kentoashketchum@gmail.com>
2022-08-16 00:11:08 +0530

    fixed

--- pages/index.tsx
+++ pages/index.tsx
@@ -40,8 +40,8 @@ const Home = ({
   }
 
   return (
-    <div className=" relative h-screen bg-gradient-to-b from-gray-900/10 to-[#010511] 
-    lg:h-[140vh] ">
+    <div className={` relative h-screen bg-gradient-to-b from-gray-900/10 to-[#010511] 
+    lg:h-[140vh] ${showmodal && '!h-screen overflow-hidden'} `}>
       <Head>
         <title>Netflix</title>
         <link rel="icon" href="/favicon.ico" />
